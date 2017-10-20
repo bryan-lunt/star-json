49fbfab1ec0e7140c29d5c22c6ece82cee325900
pedro-vicente <pedro.vicente@space-research.org>
2017-10-19 23:23:11 -0400

    update comments

--- star_json.cc
+++ star_json.cc
@@ -1,5 +1,8 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-//Center for Satellite Applications and Research (STAR), NOAA
+//Center for Satellite Applications and Research (STAR)
+//NOAA Center for Weather and Climate Prediction (NCWCP)
+//5830 University Research Court
+//College Park, MD 20740
 //Purpose: Parse a STAR JSON format file
 // see star_json.html for a description of the format
 // Dependencies: 
